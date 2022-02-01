9d2f1c4599c325d9dd9d34865bf3ac86ac5c9d3c
thomas <thomaslenguyenasian@gmail.com>
2022-01-31 20:50:13 -0800

    Add files via upload

--- JPMC-tech-task-2-PY3/src/App.tsx
+++ JPMC-tech-task-2-PY3/src/App.tsx
@@ -8,6 +8,8 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  // Added boolean called showGraph
+  showGraph: boolean,
 }
 
 /**
@@ -22,6 +24,8 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      // Set showGraph to false
+      showGraph: false,
     };
   }
 
@@ -29,18 +33,34 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
-    return (<Graph data={this.state.data}/>)
+    // Graph runs when showGraph is true
+    if (this.state.showGraph) {
+      return (<Graph data={this.state.data}/>)
+    }
   }
 
   /**
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
+    // Variable x equals 0
+    let x = 0;
+    // Constant interval
+    const interval = setInterval(() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        this.setState({
+          data: serverResponds,
+          // showGraph is set to true
+          showGraph: true,
+        });
+      });
+      // Increment by 1
+      x++;
+      // When x is greater than 1000, clears interval
+      if (x > 1000) {
+        clearInterval(interval);
+      }
+    }, 100);
   }
 
   /**
